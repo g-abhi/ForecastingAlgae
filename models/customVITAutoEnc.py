b558b8c320e5403a5f3aece820e67b243a7a129f
g-abhi <abhigvk702@gmail.com>
2023-04-30 02:13:40 -0400

    add in_channels to customvitautoenc

--- models/customVITAutoEnc.py
+++ models/customVITAutoEnc.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class CustomViTAutoEnc(nn.Module):
     def __init__(self, in_channels, img_size, patch_size, out_channels=1, deconv_chns=16, hidden_size=768, mlp_dim=3072, num_layers=12, num_heads=12, pos_embed='conv', dropout_rate=0.0, spatial_dims=2):
         super(CustomViTAutoEnc, self).__init__()
-        self.in_channels = in_channels
+        self.in_channels = in_channels # 10
         self.img_size = img_size #(128, 128)
         self.patch_size = patch_size # (16, 16)
         self.out_channels = out_channels # 20
@@ -18,6 +18,7 @@ class CustomViTAutoEnc(nn.Module):
         self.dropout_rate = dropout_rate
         self.spatial_dims = spatial_dims
         self.vitautoenc = ViTAutoEnc(
+            in_channels = self.in_channels,
             img_size = self.img_size,
             patch_size = self.patch_size,
             out_channels = self.out_channels,
